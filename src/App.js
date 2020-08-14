762e88d814faee1fdee2c223a01c1e98c0dd235f
Ouokki <ouokki98@gmail.com>
2020-08-14 17:05:56 +0100

    tables not rendred

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{useState} from 'react';
+import marked from 'marked';
+import parse from 'html-react-parser';
 import './App.css';
 
 function App() {
+  const [markedText,setmarkedText]=useState('');
+  const [value,setValue]=useState('');
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container mt-4">
+      <div className="form-group">
+          <label htmlFor="comment">Editor :</label>
+          <textarea id="editor" className="form-control" rows="5"
+          value = {value}
+          onChange={(event)=>{
+                      setValue(event.target.value)
+                   }}
+          >
+
+          </textarea>
+      </div>
+    </div> 
+    <div className="container border" id="preview">
+    <label htmlFor="comment">Previewer :</label>
+    <div
+                dangerouslySetInnerHTML={{
+                  __html: marked(value),
+                }}
+              ></div>          
     </div>
+  </> 
   );
 }
 
